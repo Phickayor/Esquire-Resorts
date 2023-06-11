8e206d26b3762ecf26c8b269145f15ce4407e54f
Fikayo Jetawo <jetawof@gmail.com>
2023-06-11 17:29:54 +0100

    starting server only

--- client/config/host.js
+++ client/config/host.js
@@ -1,2 +1,2 @@
-export const baseurl = process.env.NEXT_PUBLIC_LOCAL_URL;
-console.log(`Baseurl : ${baseurl}`)
+export const baseurl = process.env.NEXT_PUBLIC_ENVIRONMENT === "development" ? process.env.NEXT_PUBLIC_LOCAL_URL : process.env.NEXT_PUBLIC_LIVE_URL
+console.log(`Baseurl :${baseurl}`)
