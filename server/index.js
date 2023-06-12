d5c35e9ffd50e19171a54524a606bf57f5a0889b
Fikayo Jetawo <jetawof@gmail.com>
2023-06-12 12:41:08 +0100

    editiing cors in server

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
     cors({
-        origin: ["https://www.esquireresorts.com"], // restrict calls to those this address
+        origin: ["https://www.esquireresorts.com", "esquireresorts.com"], // restrict calls to those this address
         methods: ["POST"] // only allow POST requests
     })
 );
