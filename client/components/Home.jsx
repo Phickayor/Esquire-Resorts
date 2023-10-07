7dc999486fdb17d6260b8b4b7853cc9f5f3acf73
Fikayo Jetawo <jetawof@gmail.com>
2023-10-07 14:47:55 +0100

    removing bg-image from hero component

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -5,17 +5,30 @@ const italianno = Italianno({ weight: "400", subsets: ["latin"] });
 function Home() {
   const angle = <AiOutlineDoubleRight className="self-center" />;
   return (
-    <div className="top-0 h-[80vh] flex flex-col justify-center ">
-      <div className="absolute left-0 top-0 bg-slate-800">
+    <div className="bg-cover bg-opacity-40">
+      <div className="text-center py-10 lg:py-32 px-4 space-y-4 ">
+        <h1 className={`text-4xl xl:text-6xl leading-[3.5rem] font-semibold`}>
+          Discover your <span className="text-purple-500 ">Oasis </span> of
+          <span className="text-purple-500 "> Luxury</span>
+        </h1>
+        <p className="xl:text-lg font-light mx-auto lg:px-4 px-2">
+          Indulge in the epitome of sophistication and relaxation at our
+          exquisite hotel.
+        </p>
+        <div className="flex flex-col">
+          <button className="self-center gap-2 btn">Explore {angle}</button>
+        </div>
+      </div>
+      {/* <div className="absolute left-0 top-0 bg-slate-800">
         <img
           src="/images/environment1.jpg"
-          className="w-screen h-[80vh] -z-10 opacity-60 object-cover"
+          className="w-screen md:h-[80vh] -z-10 opacity-60 object-cover"
         />
-      </div>
-      <div className="gap-10 text-center relative text-white">
+      </div> */}
+      {/* <div className="gap-10 text-center relative text-black">
         <div className="lg:space-y-6 space-y-8">
           <h1 className={`text-5xl xl:text-6xl leading-[3.5rem] font-semibold`}>
-            Discover your <span className="text-purple-300 ">Oasis </span> from
+            Discover your <span className="text-purple-300 ">Oasis </span> of
             <span className="text-purple-300 "> Luxury</span>
           </h1>
           <p className="xl:text-lg font-light mx-auto lg:px-4 px-2">
@@ -26,7 +39,7 @@ function Home() {
             <button className="self-center gap-2 btn">Explore {angle}</button>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
